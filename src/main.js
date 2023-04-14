9658a083971b3c17c170b096c84d2214f32ed650
gaengbbo <gaengbbo@naver.com>
2023-04-14 19:50:06 +0900

    category 선택시 필터 구현

--- src/main.js
+++ src/main.js
@@ -1,42 +1,51 @@
-const $ul = document.querySelector('.items')
-const $nav = document.querySelector('.nav')
-const data = [];
-
 //JSON 파일에서 items를 fetch해와서 리스트로 표시해주기 
 function loadData () {
   return fetch('./data/data.json')
   .then((res) => res.json())
-  .then((data) => {
-    data = data.items;
-    data.forEach((item) => {
-      const $img = document.createElement('img');
-      $img.setAttribute("src", item.image);
-      $img.classList.add('item_thumbnail');
-      const $span = document.createElement('span');
-      $span.innerText = `${item.gender}, ${item.size}`;
-      $span.classList.add('item_description');
-      const $li = document.createElement('li');
-      $li.classList.add('item');
-      $li.appendChild($img)
-      $li.appendChild($span)
-      $ul.appendChild($li)
-    })
-  });
+  .then((json) => json.items);
+}
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  console.log(items);
+  container.innerHTML = items.map((item) => createHTML(item)).join('');
+}
+
+function createHTML(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+      <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>`;
 }
 
 
-
-function clickCategory (event) {
-  const selecShirt = document.querySelector('.shirt');
-  console.log('클릭', event.target);
-  console.log(event.target.className)
-  if ( event.target.className === 'shirt') {
-    console.log('셔츠');
+function onButtonClick (event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+  console.log(dataset);
+  if (key == null || value == null) {
+    return;
   }
+
+  displayItems(items.filter((item) => item[key] === value));
+
 }
 
-$nav.addEventListener("click", clickCategory);
 
+  
+function setEventListeners(items) {
+  const $logo = document.querySelector('.logo');
+  const $nav = document.querySelector('.nav');
+  $logo.addEventListener('click', () => {displayItems(items)});
+  $nav.addEventListener('click', (event) => onButtonClick(event, items));
+}
 
+loadData()
+ .then(items => {
+  displayItems(items);
+  setEventListeners(items);
+ })
+ .catch(console.log);
 
-loadData();
